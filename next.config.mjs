0a3d7c06dd967ac997948388aba597f6ae8a4750
Cesars117 <gilloo156@gmail.com>
2026-05-08 10:36:07 -0500

    Fix: Hostinger compatibility (standalone output and server.js)

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  // Puedes añadir más configuraciones aquí si las necesitas
+};
+
+export default nextConfig;
